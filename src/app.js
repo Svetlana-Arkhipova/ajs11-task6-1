ad07ff19bea2afd8bdc98a8b474233176483f060
Svetlana Arkhipova <sve.tochka@inbox.ru>
2021-01-07 21:50:16 +0300

    доработка и тесты

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 export default function orderByProps(obj, [...sort]) {
+  const objReplica = {};
+  Object.assign(objReplica, obj);
   const arrProps = [];
   for (let i = 0; i < sort.length; i++) {
-    for (const prop in obj) {
+    for (const prop in objReplica) {
       if (sort[i] === prop) {
-        arrProps.push({ key: prop, value: obj[prop] });
-        delete obj[prop];
+        arrProps.push({ key: prop, value: objReplica[prop] });
+        delete objReplica[prop];
       }
     }
   }
-  const restProps = Object.entries(obj);
+  const restProps = Object.entries(objReplica);
   restProps.sort();
   for (let i = 0; i < restProps.length; i++) {
     arrProps.push({ key: restProps[i][0], value: restProps[i][1] });
